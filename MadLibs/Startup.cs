99b651aca160581bc07825779876da6e10551aee
Sean Downs <spdowns@protonmail.com>
2020-07-20 14:05:33 -0700

    init project. add folder proj name with program and startup.cs and madlibs.csproj. starts to add the startup code
    Co-Authored-By: Cody Fritz <clanalia55@gmail.com>

---
+++ MadLibs/Startup.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MadLibs
+{
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
+    {
+      var builder = new ConfigurationBuilder()
+      .SetBasePath
+    }
+  }
+}
